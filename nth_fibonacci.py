77a34e3e44d55f803bafd9d93805218474f2187f
Matvey Kostukovsky <matvelius@gmail.com>
2019-05-31 23:05:37 -0500

    better solution (using memoization)

--- nth_fibonacci.py
+++ nth_fibonacci.py
@@ -1,12 +1,30 @@
-# listOfFib = [0, 1]
 
 def getNthFib(n):
+    memo = [0]*(n + 1)
+    return computeFib(n, memo)
+
+
+def computeFib(n, memo):
     if n <= 1:
         return 0
     elif n == 2:
         return 1
+    elif memo[n] > 0:
+        return memo[n]
 
-    return getNthFib(n - 1) + getNthFib(n - 2)
+    memo[n] = computeFib(n - 1, memo) + computeFib(n - 2, memo)
+
+    return memo[n]
 
 result = getNthFib(6)
 print(result)
+
+
+# basic solution (O(2^n)):
+# def getNthFib(n):
+#     if n <= 1:
+#         return 0
+#     elif n == 2:
+#         return 1
+
+#     return getNthFib(n - 1) + getNthFib(n - 2)
